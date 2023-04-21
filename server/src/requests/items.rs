28523528e3af3b8ade3227838f35d3c247037ef7
AndreaPallotta <andreapallotta.dev@gmail.com>
2023-04-20 20:16:22 -0400

    Fixed encoding parsed to value

--- server/src/requests/items.rs
+++ server/src/requests/items.rs
@@ -69,7 +69,7 @@ pub async fn get_item(
     let decoded_name = decode(name.as_str()).expect("UTF-8");
 
     let mut bind_vars: HashMap<&str, Value> = HashMap::new();
-    bind_vars.insert("name", decoded_name.into_owned());
+    bind_vars.insert("name", Value::String(decoded_name.into_owned()));
 
     match database.arango_db.aql_bind_vars("FOR item IN Item FILTER LOWER(item.name) LIKE CONCAT('%', LOWER(@name), '%') RETURN item", bind_vars).await {
         Ok(items) => {
